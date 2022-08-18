8652dfda8db42d8ec6ef7c2fbcf3441d651f22a5
Jesus Pernia <padron2991@gmail.com>
2022-08-18 14:54:21 -0500

    views

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    #ex: /polls/
+    path("", views.index, name="index"), #<int:question_id/> es  la manera que da jango de poder pasar parametros variables metoante la url que nosotros estamos utilizado para una pagina
+    #ex: /polls/5/
+    path("<int:question_id>/detail/", views.detail, name="detail"),
+    #ex: /polls/5/result
+    path("<int:question_id>/results/", views.results, name="results"),
+    #ex: /polls/5/vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from urllib.parse import quote_from_bytes
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect # es una clase que permite ejecutar un respuesta http 
+from django.urls import reverse
 
-from django.http import HttpResponse # es una clase que permite ejecutar un respuesta http 
+from .models import Question, Choice
 
-def index(request):
-    return HttpResponse("Hellor Word")
+def index(request): # una vista siempre reciben un objeto de tipo httprequest como parametro cliente 
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question =  get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html",{
+        "question": question
+    })
+    
+    
+def results(request, question_id):
+    question =  get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question":question
+    })
+
+
+def vote(request, question_id):
+    question =  get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html",{
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
